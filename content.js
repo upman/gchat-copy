b39314552d13c679a78f85803f2db85067e42002
rlazar <ravi.kumar.lazar@zalando.de>
2020-10-07 14:05:14 +0200

    Add quote message button

--- content.js
+++ content.js
@@ -112,11 +112,13 @@ function addStyle() {
 function main() {
     var scrollContainer = document.querySelector('c-wiz[data-group-id][data-is-client-side] > div:nth-child(1)');
     var copyButtonInsertedCount = 0;
+    // Iterating on threads and in the case of DMs, the whole message history is one thread
     document.querySelectorAll("c-wiz[data-topic-id]")
         .forEach(
             function(e,t,i){
                 var copy = e.querySelector('.gchat-xtra-copy');
                 if(e.getAttribute("data-topic-id") && !copy){
+                    // Adding copy thread link buttons to thread
                     var copyButton = document.createElement("div");
                     copyButton.className="gchat-xtra-copy";
                     copyButton.innerHTML = `
@@ -154,11 +156,139 @@ function main() {
                         buttonContainer.parentElement.parentElement.parentElement.parentElement.style = 'padding-top: 56px;';
                     }
                 }
+
+                // Iterating on each message in the thread
+                e.querySelectorAll('div[data-tooltip="Add reaction"]').forEach(
+                    // Adding quote message buttons
+                    function(addreactionButton) {
+                        if (addreactionButton.parentElement.parentElement.querySelector('[data-tooltip*="Quote Message"')) {
+                            return;
+                        }
+                        const container = document.createElement('div');
+                        // Quote svg icon
+                        container.innerHTML = `
+                            <svg viewBox="0 0 24 24" width="24px" height="24px" xmlns="http://www.w3.org/2000/svg" style="margin-top: 4px">
+                                <path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm0 8v3.701c0 2.857-1.869 4.779-4.5 5.299l-.498-1.063c1.219-.459 2.001-1.822 2.001-2.929h-2.003v-5.008h5zm6 0v3.701c0 2.857-1.869 4.779-4.5 5.299l-.498-1.063c1.219-.459 2.001-1.822 2.001-2.929h-2.003v-5.008h5z"/>
+                            </svg>`;
+                        container.className=addreactionButton.className;
+                        container.setAttribute('data-tooltip', 'Quote Message');
+                        const quoteSVG = container.children[0]
+                        addreactionButton.querySelector('svg').classList.forEach(c => quoteSVG.classList.add(c));
+
+                        var elRef = addreactionButton;
+                        // Find parent container of the message
+                        // These messages are then grouped together when they are from the recipient
+                        // and the upper most one has the name and time of the message
+                        while(!(elRef.className && elRef.className.includes('nF6pT')) && elRef.parentElement) {
+                            elRef = elRef.parentElement;
+                        }
+                        if (elRef.className.includes('nF6pT')) {
+
+                            var messageIndex, name;
+                            [...elRef.parentElement.children].forEach((messageEl, index) => {
+                                if (messageEl ===  elRef) {
+                                    messageIndex = index;
+                                }
+                            });
+
+                            addreactionButton.parentElement.parentElement.appendChild(container);
+                            container.addEventListener('click', () => {
+                                while (messageIndex >= 0) {
+                                    if (elRef.parentElement.children[messageIndex].className.includes('AnmYv')) {
+                                        const nameContainer = elRef.parentElement.children[messageIndex].querySelector('[data-hovercard-id], [data-member-id]');
+                                        name = nameContainer.getAttribute('data-name');
+                                        break;
+                                        // Can extract time, but adding it into static text surrounded by relative time that's rendered in the chats will only confuse people
+                                        // time = el.Ref.parentElement.children[messageIndex].querySelector('span[data-absolute-timestamp]').getAttribute('data-absolute-timestamp');
+                                    }
+                                    messageIndex -= 1;
+                                }
+
+                                var messageContainer = addreactionButton.parentElement.parentElement.parentElement.parentElement.children[0];
+                                var quoteText = getQuoteText(messageContainer);
+
+                                let inputEl = e.querySelector('div[contenteditable="true"]'); // This fetches the input element in channels
+                                let dmInput = document.body.querySelectorAll('div[contenteditable="true"]'); // This fetches the input in DMs
+                                inputEl = inputEl ? inputEl : dmInput[dmInput.length - 1];
+                                if (!inputEl) {
+                                    return;
+                                }
+                                inputEl.innerHTML = "```\n" + name + ":\n" + quoteText + "\n```\n" + inputEl.innerHTML;
+                                inputEl.scrollIntoView();
+                                inputEl.click();
+                                placeCaretAtEnd(inputEl);
+                            });
+                        }
+                    }
+                );
             }
         );
 
     if (copyButtonInsertedCount > 1) {
         scrollContainer.scrollTop += 36;
+    }
+}
+
+function getQuoteText(messageContainer) {
+    var regularText = getText(messageContainer);
+    var videoCall = messageContainer.querySelector('a[href*="https://meet.google.com/"]');
+    var image = messageContainer.querySelector('a img[alt]');
+    var text = regularText ||
+        (videoCall ? "🎥: " + videoCall.href: null) ||
+        (image ? "📷: " + image.alt: null) ||
+        '...';
+
+    return truncateQuoteText(text);
+}
+
+function truncateQuoteText(text) {
+    let splitText = text.split('\n');
+    let quoteText = splitText.slice(0,3).join('\n') + (splitText.length > 3 ? '\n...' : '');
+    if (quoteText.length > 280) {
+        quoteText = quoteText.slice(0, 280) + ' ...';
+    }
+    return quoteText;
+}
+
+function getText(messageContainer) {
+    const multilineMarkdownClass = 'FMTudf';
+    let textContent = '';
+    const childNodes = messageContainer.children[0].childNodes;
+    for (var i = 0; i < childNodes.length; i += 1) {
+        if (childNodes[i].nodeType === Node.TEXT_NODE) {
+            textContent += childNodes[i].textContent;
+        } else if (childNodes[i].className === 'jn351e') {
+            continue;
+        } else if (childNodes[i].className === multilineMarkdownClass) {
+            textContent += '...\n';
+        } else if (childNodes[i].tagName === 'IMG') {
+            // emojis
+            textContent += childNodes[i].alt;
+        } else {
+            textContent += childNodes[i].innerHTML;
+        }
+    }
+
+    return textContent;
+}
+
+function placeCaretAtEnd(el) {
+    el.focus();
+    if (typeof window.getSelection != "undefined"
+            && typeof document.createRange != "undefined") {
+        var range = document.createRange();
+        range.selectNodeContents(el);
+        range.collapse(false);
+        var sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+        range.insertNode(document.createElement('br'));
+        range.collapse();
+    } else if (typeof document.body.createTextRange != "undefined") {
+        var textRange = document.body.createTextRange();
+        textRange.moveToElementText(el);
+        textRange.collapse(false);
+        textRange.select();
     }
 }
 
@@ -179,4 +309,4 @@ function debounce(fn, delay) {
 addStyle();
 main();
 var el = document.documentElement;
-el.addEventListener('DOMSubtreeModified', debounce(main, 1000));
+el.addEventListener('DOMSubtreeModified', debounce(main, 2000));
