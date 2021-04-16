7ced4aabdb03ffa0211ac37f7dd5080988f38d5f
teejay-87 <tommasosala87@gmail.com>
2021-04-16 17:09:42 +0200

    Update tampermonkey.user.js

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,6 +1,8 @@
 // ==UserScript==
 // @name         Google Chat thread links & quote reply
 // @version      0.0.1
+// @updateURL    https://raw.githubusercontent.com/teejay-87/gchat-copy/master/tampermonkey.meta.js
+// @downloadURL  https://raw.githubusercontent.com/teejay-87/gchat-copy/master/tampermonkey.user.js
 // @description  Adds button to copy links to threads on Google Chat and adds button to messages to quote reply
 // @author       upman
 // @include      https://chat.google.com/*
